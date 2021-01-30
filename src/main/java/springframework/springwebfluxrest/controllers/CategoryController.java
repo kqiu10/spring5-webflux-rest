c580112065103edc871ac170e78d4090e8c023f7
kqiu10 <qiukan97@gmail.com>
2021-01-30 11:33:17 -0500

    create CategoryController

---
+++ src/main/java/springframework/springwebfluxrest/controllers/CategoryController.java
@@ -0,0 +1,31 @@
+package springframework.springwebfluxrest.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import springframework.springwebfluxrest.domain.Category;
+import springframework.springwebfluxrest.repositories.CategoryRepository;
+
+/**
+ * Date: 1/30/21
+ * Question Description
+ */
+
+public class CategoryController {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping("/api/v1/categories")
+    Flux<Category> list() {
+        return categoryRepository.findAll();
+    }
+
+    @GetMapping("/api/v1/categories/{id}")
+    Mono<Category> getById(@PathVariable String id) {
+        return categoryRepository.findById(id);
+    }
+}
