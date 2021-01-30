183c7394a282b8983dd7ef8b3e8047e9e55d0aa1
kqiu10 <qiukan97@gmail.com>
2021-01-30 12:13:36 -0500

    created testing for category controller bug in test needs to be fixed

--- src/main/java/springframework/springwebfluxrest/controllers/CategoryController.java
+++ src/main/java/springframework/springwebfluxrest/controllers/CategoryController.java
@@ -2,6 +2,7 @@ package springframework.springwebfluxrest.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import springframework.springwebfluxrest.domain.Category;
@@ -11,14 +12,13 @@ import springframework.springwebfluxrest.repositories.CategoryRepository;
  * Date: 1/30/21
  * Question Description
  */
-
+@RestController
 public class CategoryController {
     private final CategoryRepository categoryRepository;
 
     public CategoryController(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
     }
-
     @GetMapping("/api/v1/categories")
     Flux<Category> list() {
         return categoryRepository.findAll();
---
+++ src/test/java/springframework/springwebfluxrest/controllers/CategoryControllerTest.java
@@ -0,0 +1,39 @@
+package springframework.springwebfluxrest.controllers;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import springframework.springwebfluxrest.domain.Category;
+import springframework.springwebfluxrest.repositories.CategoryRepository;
+
+public class CategoryControllerTest {
+
+    WebTestClient webTestClient;
+    CategoryRepository categoryRepository;
+    CategoryController categoryController;
+    @Before
+    public void setUp() throws Exception {
+        categoryRepository = Mockito.mock(CategoryRepository.class);
+        categoryController = new CategoryController(categoryRepository);
+        webTestClient = WebTestClient.bindToController(categoryController).build();
+    }
+    @Test
+    public void list() {
+        BDDMockito.given(categoryRepository.findAll()).willReturn(Flux.just(Category.builder().description("Cat1").build(),
+        Category.builder().description("Cat2").build()));
+
+        webTestClient.get().uri("/api/v1/categories/").exchange().expectBodyList(Category.class).hasSize(2);
+
+    }
+
+    @Test
+    void getById() {
+        BDDMockito.given(categoryRepository.findById("someid")).willReturn(Mono.just(Category.builder().description("Cat").build()));
+
+        WebTestClient.get().uri("/api/v1/categories/someid").exchange().expectBody(Category.class);
+    }
+}
